21da2083cfbce73e289fbad612eefe6de4be1b8c
Cynthia <cynthiaphan98@gmail.com>
2023-02-27 12:39:10 -0500

    test

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,9 @@ public class Main {
      *
      * @param args
      */
+
+    // test
+
     public static void main(String[] args) {
         String errorLog = "Erreurs:";
         GuildCommandSystem guildCommandSystem = new GuildCommandSystem(args);
